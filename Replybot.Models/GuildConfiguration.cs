856622cdc0a356b0ce65af4cf9fed2a129f6b7e7
Robert R <rarDevelopment@users.noreply.github.com>
2023-04-23 01:16:43 -0400

    removing folder change from workflow

--- Replybot.Models/GuildConfiguration.cs
+++ Replybot.Models/GuildConfiguration.cs
@@ -1,12 +1,11 @@
-﻿namespace Replybot.Models
+﻿namespace Replybot.Models;
+
+public class GuildConfiguration
 {
-    public class GuildConfiguration
-    {
-        public string GuildId { get; set; }
-        public string? GuildName { get; set; }
-        public bool EnableAvatarAnnouncements { get; set; }
-        public bool EnableAvatarMentions { get; set; }
-        public string? LogChannelId { get; set; }
-        public List<string> AdminUserIds { get; set; }
-    }
+    public string GuildId { get; set; }
+    public string? GuildName { get; set; }
+    public bool EnableAvatarAnnouncements { get; set; }
+    public bool EnableAvatarMentions { get; set; }
+    public string? LogChannelId { get; set; }
+    public List<string> AdminUserIds { get; set; }
 }
