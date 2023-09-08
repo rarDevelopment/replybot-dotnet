a2235183817a13e4bb9e5d0e78998849161f7460
Robert R <rarDevelopment@users.noreply.github.com>
2023-09-08 18:33:25 -0400

    add site ignore list for the previously posted links feature

---
+++ Replybot.ServiceLayer/SiteIgnoreService.cs
@@ -0,0 +1,26 @@
+﻿using Replybot.Models;
+
+namespace Replybot.ServiceLayer
+{
+    public class SiteIgnoreService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SiteIgnoreService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string?> GetSiteIgnoreList()
+        {
+            var client = _httpClientFactory.CreateClient(HttpClients.SiteIgnoreList.ToString());
+            var response = await client.GetAsync("");
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            var siteIgnoreList = await response.Content.ReadAsStringAsync();
+            return siteIgnoreList;
+        }
+    }
+}
