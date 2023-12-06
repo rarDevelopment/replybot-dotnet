2534cc77227d766108d557dca4752fc34295e1f5
Robert R <rarDevelopment@users.noreply.github.com>
2023-12-05 20:15:27 -0500

    fix tiktok links

--- Replybot/ReactionCommands/FixTikTokCommand.cs
+++ Replybot/ReactionCommands/FixTikTokCommand.cs
@@ -6,8 +6,8 @@ namespace Replybot.ReactionCommands;
 public class FixTikTokCommand(BotSettings botSettings) : FixUrlCommandBase(FixLinkConfig, botSettings), IReactionCommand
 {
     public readonly string NoLinkMessage = "I don't think there's a TikTok link there.";
-    private const string TikTokUrlRegexPattern = "https?:\\/\\/(vm.)?(tiktok.com)/[a-z0-9-_//]+";
-    private const string VxTikTokUrlRegexPattern = "https?:\\/\\/(vm.)?(vxtiktok.com)/[a-z0-9-_//]+";
+    private const string TikTokUrlRegexPattern = "https?:\\/\\/(vm.|www.)?(tiktok.com)/[\\@a-z0-9-_//]+";
+    private const string VxTikTokUrlRegexPattern = "https?:\\/\\/(vm.|www.)?(vxtiktok.com)/[\\@a-z0-9-_//]+";
     public const string FixTikTokButtonEmojiId = "1177645488858742844";
     public const string FixTikTokButtonEmojiName = "fixtiktok";
     private const string OriginalTikTokBaseUrl = "tiktok.com";
