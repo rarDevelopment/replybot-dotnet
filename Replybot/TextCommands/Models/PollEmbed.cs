f78565b18f3f2a83be6c425299c32583ec02434e
Robert R <rarDevelopment@users.noreply.github.com>
2024-05-14 23:35:47 -0400

    remove poll feature

--- Replybot/TextCommands/Models/PollEmbed.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Replybot.TextCommands.Models;
-
-public class PollEmbed(Embed embed, IReadOnlyList<IEmote>? reactionEmotes = null)
-{
-    public Embed Embed { get; set; } = embed;
-    public IReadOnlyList<IEmote>? ReactionEmotes { get; set; } = reactionEmotes;
-}
