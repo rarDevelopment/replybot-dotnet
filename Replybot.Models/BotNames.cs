ee5063f8734400db4bf7df7e6ee8cbddd499c8bd
basilscode <69129137+basilscode@users.noreply.github.com>
2024-08-13 20:10:43 -0500

    added names

--- Replybot.Models/BotNames.cs
+++ Replybot.Models/BotNames.cs
@@ -5,6 +5,6 @@ namespace Replybot.Models;
 [ExcludeFromCodeCoverage]
 public static class BotNames
 {
-    public static string[] Names { get; } = ["toby", "replybot", "tobias", "tobster"];
+    public static string[] Names { get; } = ["toby", "replybot", "tobias", "tobster", "tobiathan", "tobicus"];
     public static string[] NamesToIgnore { get; } = ["toby fox", "replybot-dotnet"];
 }
