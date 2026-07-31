2c35e2dd242feff37736f8616fc61afdc6217edc
rarDevelopment <rarDevelopment@users.noreply.github.com>
2026-07-30 21:52:44 -0400

    fix 5 day check oops

--- Replybot/NotificationHandlers/MessageReceivedNotificationHandler.cs
+++ Replybot/NotificationHandlers/MessageReceivedNotificationHandler.cs
@@ -240,8 +240,12 @@ public class MessageReceivedNotificationHandler(IReplyBusinessLayer replyBusines
                 continue;
             }
             if (messageWithLinks.Author.Id == relevantMessage.Author.Id &&
-                (messageWithLinks.Timestamp - relevantMessage.Timestamp < TimeSpan.FromMinutes(5)
-                || messageWithLinks.Timestamp - relevantMessage.Timestamp > TimeSpan.FromDays(5)))
+                messageWithLinks.Timestamp - relevantMessage.Timestamp < TimeSpan.FromMinutes(5))
+            {
+                continue;
+            }
+            if(messageWithLinks.Author.Id != relevantMessage.Author.Id
+               && messageWithLinks.Timestamp - relevantMessage.Timestamp > TimeSpan.FromDays(5))
             {
                 continue;
             }
